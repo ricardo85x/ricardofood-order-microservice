8a527339f8acf019bae779593d9324436af770d0
ricardo <ricardo85x@gmail.com>
2024-08-09 18:45:22 -0300

    feat: move PaymentQueueName to constant package and add order info constant

--- src/main/java/com/ricardofood/order/enums/PaymentQueueName.java
+++
@@ -1,7 +0,0 @@
-package com.ricardofood.order.enums;
-
-public class PaymentQueueName {
-    public static final String PAYMENT_COMPLETED = "payment.completed";
-    public static final String PAYMENT_FAILED = "payment.failed";
-}
-
