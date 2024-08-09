b1fc153467ad195c9f8b8f9891ee52691c74dcaa
ricardo <ricardo85x@gmail.com>
2024-08-09 12:24:24 -0300

    feat: format the message in the listener

--- src/main/java/com/ricardofood/order/amqp/PaymentListener.java
+++ src/main/java/com/ricardofood/order/amqp/PaymentListener.java
@@ -1,5 +1,6 @@
 package com.ricardofood.order.amqp;
 
+import com.ricardofood.order.dto.PaymentDto;
 import com.ricardofood.order.enums.PaymentQueueName;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -9,7 +10,18 @@ import org.springframework.stereotype.Component;
 public class PaymentListener {
 
     @RabbitListener(queues = PaymentQueueName.PAYMENT_COMPLETED)
-    public void receiveMessage(Message message) {
-        System.out.println("Received message: " + new String(message.getBody()));
+    public void receiveMessage(PaymentDto paymentDto) {
+        var message = """
+            Payment Id: %s
+            Order Number: %s
+            Amount: R$: %s
+            Status: %s
+        """.formatted(
+                paymentDto.getId(),
+                paymentDto.getOrderId(),
+                paymentDto.getAmount(),
+                paymentDto.getStatus()
+        );
+        System.out.println("Received message: \n" + message);
     }
 }
